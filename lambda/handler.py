79cfd0d82900d5ff4d471cd66fba1ad26463edd7
luispastor <Luis.Pastor@alu.uclm.es>
2022-11-04 14:00:08 +0100

    first commit

---
+++ lambda/handler.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+
+client = boto3.client('dynamodb')
+MY_TABLE = os.environ['ENV_MYTABLE']
+
+def lambda_handler(event, context):
+    
+    response = client.update_item(
+        TableName = MY_TABLE,
+        Key = {
+            'id': {'S': 'my_website'}
+        },
+        UpdateExpression = 'ADD visitors :inc',
+        ExpressionAttributeValues = {':inc' : {'N': '1'}},
+        ReturnValues = 'UPDATED_NEW'
+        )
+        
+    value = response['Attributes']['visitors']['N']
+    
+    return {      
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'statusCode': 200,
+            'body': {'visits':f'{value}'}
+    }
